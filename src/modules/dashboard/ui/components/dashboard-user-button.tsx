179862c1a91ef1fb4768e32864627f2818e7067c
Krisno Mukti <krsnmvk@gmail.com>
2025-06-08 15:38:49 +0700

    feat: dashbord navbar

--- src/modules/dashboard/ui/components/dashboard-user-button.tsx
+++ src/modules/dashboard/ui/components/dashboard-user-button.tsx
@@ -4,6 +4,18 @@ import GeneratedAvatar from '@/app/_components/generated-avatar';
 
 import { Avatar } from '@/components/ui/avatar';
 
+import { Button } from '@/components/ui/button';
+
+import {
+  Drawer,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from '@/components/ui/drawer';
+
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -13,6 +25,8 @@ import {
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 
+import { useIsMobile } from '@/hooks/use-mobile';
+
 import { authClient } from '@/lib/auth-client';
 
 import { AvatarImage } from '@radix-ui/react-avatar';
@@ -21,11 +35,61 @@ import { ChevronDownIcon, CreditCardIcon, LogOutIcon } from 'lucide-react';
 
 import { useRouter } from 'next/navigation';
 
-export default function UserButton() {
+export default function DashboardUserButton() {
   const { data, isPending } = authClient.useSession();
+
   const router = useRouter();
 
+  async function onLogout() {
+    await authClient.signOut({
+      fetchOptions: { onSuccess: () => router.push('/sign-in') },
+    });
+  }
+
+  const isMobile = useIsMobile();
+
   if (!data || isPending) return null;
+
+  if (isMobile) {
+    return (
+      <Drawer>
+        <DrawerTrigger className="rounded-lg border border-border/10 w-full flex items-center justify-between bg-white/5 hover:bg-white/10 p-3 overflow-hidden">
+          <Avatar>
+            {data.user.image ? (
+              <AvatarImage src={data.user.image} alt={data.user.name} />
+            ) : (
+              <GeneratedAvatar
+                seed={data.user.name}
+                variant="initials"
+                className="size-8"
+              />
+            )}
+          </Avatar>
+          <div className="flex ml-3 flex-col gap-0.5 overflow-hidden flex-1 text-left min-w-0">
+            <h4 className="text-sm truncate w-full">{data.user.name}</h4>
+            <h5 className="text-xs truncate w-full">{data.user.email}</h5>
+          </div>
+          <ChevronDownIcon className="size-4 shrink-0" />
+        </DrawerTrigger>
+        <DrawerContent>
+          <DrawerHeader>
+            <DrawerTitle>{data.user.name}</DrawerTitle>
+            <DrawerDescription>{data.user.email}</DrawerDescription>
+          </DrawerHeader>
+          <DrawerFooter>
+            <Button type="button" variant="ghost">
+              <span>Billing</span>
+              <CreditCardIcon className="size-4" />
+            </Button>
+            <Button type="button" onClick={onLogout} variant="ghost">
+              <span>Logout</span>
+              <LogOutIcon className="size-4" />
+            </Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    );
+  }
 
   return (
     <DropdownMenu>
@@ -64,11 +128,7 @@ export default function UserButton() {
           <CreditCardIcon className="size-4" />
         </DropdownMenuItem>
         <DropdownMenuItem
-          onClick={async () =>
-            await authClient.signOut({
-              fetchOptions: { onSuccess: () => router.push('/sign-in') },
-            })
-          }
+          onClick={onLogout}
           className="flex items-center justify-between cursor-pointer"
         >
           <span>Logout</span>
